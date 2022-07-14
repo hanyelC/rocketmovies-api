ad00a6ff0b1a229ff7d950d8bb770c1a4263a64f
hanyelC <hanyel.chamon@gmail.com>
2022-07-14 14:06:27 -0300

    reafactor(services): creating UserCreateService

---
+++ src/services/UserCreateService.js
@@ -0,0 +1,26 @@
+const { hashSync } = require("bcryptjs")
+const AppError = require("../utils/AppError")
+
+class UserCreateService {
+  constructor(userRepository) {
+    this.userRepository = userRepository
+  }
+
+  async execute({ name, email, password }) {
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      throw new AppError("Formato de email inválido")
+    }
+
+    const checkIfUserExists = await this.userRepository.findByEmail(email)
+
+    if (checkIfUserExists) {
+      throw new AppError("Email já cadastrado")
+    }
+
+    const hashedPassword = hashSync(password, 8)
+
+    await this.userRepository.create({ name, email, password: hashedPassword })
+  }
+}
+
+module.exports = { UserCreateService }
