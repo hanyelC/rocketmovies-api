766316e3f9a80e108f2bed0e4b64c0d59a90291a
hanyelC <hanyel.chamon@gmail.com>
2022-07-14 20:23:52 -0300

    refactor(UserRepository): move db interaction from UserController to UserRepository

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -6,7 +6,17 @@ class UserRepository {
 
     const user = await db.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    db.close()
+    await db.close()
+
+    return user
+  }
+
+  async findById(id) {
+    const db = await sqliteConnection()
+
+    const user = await db.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    await db.close()
 
     return user
   }
@@ -16,9 +26,19 @@ class UserRepository {
 
     const userId = await db.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, password])
 
-    db.close()
+    await db.close()
 
     return { id: userId }
+  }
+
+  async update({ id, name, email, password }) {
+    const db = await sqliteConnection()
+
+    await db.run("UPDATE users SET name = ?, email = ?, password = ?, updated_at = DATETIME('now')  WHERE id = ?", [name, email, password, id])
+
+    await db.close()
+
+    return { id }
   }
 }
 
